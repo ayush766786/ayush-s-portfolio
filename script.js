204c82c3c295244430e765e4d635918fc22b4d30
ayush766786 <jayushkumar211@gmail.com>
2024-08-25 13:11:48 +0530

    about section completed

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let p=document.getElementsByClassName("p")
-let button=document.getElementsByClassName("btn")
 
-button.addEventListener("click",()=>{
-  p.classList.toggle("active")
-  console.log(click)
+
+let span = document.querySelector(".span")
+console.log(span)
+let button = document.querySelector(".btn")
+console.log(button)
+
+button.addEventListener("click", () => {
+
+  span.classList.toggle("active")
+
 })
