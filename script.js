452ebb14970bb45b4f76268ef188d350e4168738
ayush766786 <jayushkumar211@gmail.com>
2024-08-26 01:38:47 +0530

    scroll reval completed

--- script.js
+++ script.js
@@ -51,3 +51,15 @@ window.onscroll = () => {
   navbar.classList.remove("active")
 }
 
+// scroll reval
+ScrollReveal({ 
+  reset: true,
+  distance:"80px",
+  duration:2000,
+  delay:200 
+
+});
+ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ScrollReveal().reveal('.home-img, .services-container, .project-box, .contact form', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin:'left' });
+ScrollReveal().reveal('.home-content p, .about-content ', { origin:'right' });
