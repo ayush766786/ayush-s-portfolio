2525a7875693f7db3fed8c24b23ca22e22867626
ayush766786 <jayushkumar211@gmail.com>
2024-08-26 00:16:20 +0530

    complete nav bar

--- script.js
+++ script.js
@@ -11,24 +11,43 @@ button.addEventListener("click", () => {
 
 })
 
-let sections=document.querySelectorAll("section");
-console.log(sections)
-let navLinks=document.querySelectorAll("header nav a")
-console.log(navLinks)
+// toggle icon navBar
 
-window.onscroll=()=>{
-  sections.forEach((sec)=>{
-    let top=window.scrollY;
-    let offset=sec.offsetTop - 150;
-    let height=sec.offsetHeight;
-    let id=sec.getAttribute('id')
-     
-    if(top>=offset && top<offset+height){
-      navLinks.forEach((links)=>{
+let menuIcon = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".navbar")
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle(`fa-x`)
+  navbar.classList.toggle("active")
+}
+
+// scroll section active links
+
+let sections = document.querySelectorAll("section");
+// console.log(sections)
+let navLinks = document.querySelectorAll("header nav a")
+// console.log(navLinks)
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
         links.classList.remove('active');
-        document.querySelector(`header nav a[href*=`+id+`]`).classList.add('active')
+        document.querySelector(`header nav a[href*=` + id + `]`).classList.add('active')
       })
     }
   })
+  // sticky navbar
+  let header = document.querySelector('header');
+  console.log(header)
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  // remove toggle icon and navbar
+  menuIcon.classList.remove(`fa-x`)
+  navbar.classList.remove("active")
 }
 
